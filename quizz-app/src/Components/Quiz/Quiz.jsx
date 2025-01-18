561a0ceb3406a9ab49029b4bf084152c42b0d6a5
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-01-18 11:19:52 +0300

    Update Quiz.jsx

--- quizz-app/src/Components/Quiz/Quiz.jsx
+++ quizz-app/src/Components/Quiz/Quiz.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import "./Quiz.css";
 import { data } from "../../assets/data";
 
-const Quiz = () => {
+const Quiz = () => {s
   let [index, setİndex] = useState(0); 
   let [question, setQuestion] = useState(data[index]); /.
   let [lock,setLock] = useState(false); 
